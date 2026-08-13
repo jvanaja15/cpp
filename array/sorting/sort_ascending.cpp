f466d7a86f2a86dc5a2cccc2b02ca0fe994cb637
vanaja J <vanajaj15@zohomail.in>
2026-08-13 09:28:25 -0700

    sorting ascending

---
+++ array/sorting/sort_ascending.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={20,30,10,40,50};
+    int n=5;
+    int temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
