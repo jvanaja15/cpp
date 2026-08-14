09b8d3a38b5c674c5710e655c407c8889e42ccc8
vanaja J <vanajaj15@zohomail.in>
2026-08-14 06:31:21 -0700

    right rotate using reverse

---
+++ array/rotate/r_rotate_rev.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void reverse(int arr[],int start,int end)
+{
+while(start<end)
+{ 
+    int temp=arr[start];
+     arr[start]=arr[end];
+    arr[end]=temp;
+    start++;
+    end--;
+}
+}
+int main()
+{
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int n=10;
+    int k=3;
+    k=k%n;
+    reverse(arr,0,n-k-1);
+    reverse(arr,n-k,n-1);
+    reverse(arr,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
