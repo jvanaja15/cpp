3bdb34bf625c572701a3bf17a746f778dbd2a552
vanaja J <vanajaj15@zohomail.in>
2026-09-10 08:37:59 -0700

    bank with validation using encapsulation

---
+++ encapsulation/bank_encap.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Bank
+{
+private:
+int balance;
+public:
+
+void setBalance(int b)
+{
+    if(b>0)
+    {
+       balance=b;
+    }
+  
+}
+int getBalance()
+{
+    return balance;
+}
+};
+int main()
+{
+    Bank b;
+    b.setBalance(5000);
+    cout<<"Marks:"<<b.getBalance();
+    return 0;
+}
